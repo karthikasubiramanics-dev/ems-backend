b422ae0ee73634c6b6d9053381bbd3f6f4f80661
Karthika <karthika.avirtueskarthika.s@avirtues.com>
2026-05-01 20:24:05 +0530

    backend final fix

--- src/main/java/com/Avirtues/ems/dto/EmployeeDTO.java
+++ src/main/java/com/Avirtues/ems/dto/EmployeeDTO.java
@@ -1,38 +1,25 @@
 package com.Avirtues.ems.dto;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
 
     private Long id;
+
+    // Changed from name → firstName + lastName
     private String firstName;
     private String lastName;
+
     private String email;
 
-    public EmployeeDTO() {
-    }
-
-    public EmployeeDTO(Long id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // Added missing fields
+    private String department;
+    private double salary;
 }
